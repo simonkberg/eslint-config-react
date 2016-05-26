7e770f5cc4a603e4d42eb9d3fc49a9cbf64b6a54
Simon Kjellberg <simon.kjellberg@gmail.com>
2016-05-26 16:19:23 +0200

    add test spec

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-'use strict'
-
-module.exports = {
-  parser: 'babel-eslint',
-  extends: ['standard', 'plugin:react/recommended'],
-  plugins: ['react', 'babel'],
-  env: {
-    'browser': true
-  }
-}
+module.exports = require('./eslintrc.json')
---
+++ spec/indexSpec.js
@@ -0,0 +1,46 @@
+/* eslint-env jasmine */
+
+const eslintrc = require('../')
+const { CLIEngine } = require('eslint')
+const { isArray } = Array
+
+describe('@simonkberg/eslint-config-react', () => {
+  it('should return an object', () => {
+    expect(typeof eslintrc).toEqual('object')
+  })
+
+  it('should use `babel-eslint` as parser', () => {
+    expect(eslintrc.parser).toBeDefined()
+    expect(eslintrc.parser).toEqual('babel-eslint')
+  })
+
+  it('should extend `standard` and `standard-react`', () => {
+    expect(isArray(eslintrc.extends)).toEqual(true)
+
+    expect(eslintrc.extends).toContain('standard')
+    expect(eslintrc.extends).toContain('standard-react')
+  })
+
+  it('should use `babel` plugin', () => {
+    expect(isArray(eslintrc.plugins)).toEqual(true)
+    expect(eslintrc.plugins).toContain('babel')
+  })
+
+  it('should define `browser` as environment', () => {
+    expect(typeof eslintrc.env).toEqual('object')
+    expect(eslintrc.env.browser).toEqual(true)
+  })
+
+  it('should load config in eslint', () => {
+    const cli = new CLIEngine({
+      useEslintrc: false,
+      configFile: 'eslintrc.json'
+    })
+
+    const stub = 'const foo = 1\nconst bar = baz => baz\nbar(foo)\n'
+    const result = cli.executeOnText(stub)
+
+    expect(result.errorCount).toEqual(0)
+    expect(result.warningCount).toEqual(0)
+  })
+})
